536d75c8bda4f0f6f0a7ed7428725f540bf05f06
Georg Maißer <georg.maisser@wunderbyte.at>
2024-02-10 22:54:18 +0100

    Improvement: Add new table to store configs for forms.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024020900;
+$plugin->version = 2024021000;
 $plugin->requires = 2022041900; // Requires this Moodle version. Current: Moodle 4.0.0.
 $plugin->release = '8.1.14';
 $plugin->maturity = MATURITY_STABLE;
